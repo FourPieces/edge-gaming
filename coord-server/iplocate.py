abaa64052b8941b58650381ac0e05952ef5a4631
Skyler Bistarkey <sdbxenon@gmail.com>
2019-02-21 08:59:52 -0800

    Begone, tabs

--- coord-server/iplocate.py
+++ coord-server/iplocate.py
@@ -2,18 +2,18 @@ import requests
 
 # Simple interface to get a location from an IP address
 def get_location(ip_addr):
-	r = requests.get("http://ip-api.com/json/" + ip_addr)
+  r = requests.get("http://ip-api.com/json/" + ip_addr)
 
-	if r.status_code != 200:
-		return None
+  if r.status_code != 200:
+    return None
 
-	return r.json()
+  return r.json()
 
 def get_location_str(ip_addr):
-	loc = get_location(ip_addr)
+  loc = get_location(ip_addr)
 
-	try:
-		return loc['city'] + ", " + loc['region'] + ", " + loc['country']
+  try:
+    return loc['city'] + ", " + loc['region'] + ", " + loc['country']
 
-	except:
-		return "Location not found."
+  except:
+    return "Location not found."
